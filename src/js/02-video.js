6d5d4b31ba21e535a5a1e640b0bf7508a9bd9742
Dranyk <dvdranyk@gmail.com>
2022-11-16 16:47:45 +0200

    2- done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const onPlay = ({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+const iframe  = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));

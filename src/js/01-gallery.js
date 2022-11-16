d9e4677e471d34d54f1dafa7951e509a1295a2ad
Dranyk <dvdranyk@gmail.com>
2022-11-16 16:32:35 +0200

    1- done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,54 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+addAttributesForSimpleLightbox(galleryContainer);
+addLazyloadToImg();
+const lightbox = new SimpleLightbox(".gallery > a", { captionDelay: 250 });
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a 
+        class="gallery__item" 
+      href="${original}">
+     <img class="gallery__image" 
+    src="${preview}" 
+     alt="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+
+function addAttributesForSimpleLightbox(galleryEl) {
+  [...galleryEl.getElementsByTagName("a")].map((el) => {
+    const imgEl = el.getElementsByTagName("img")[0];
+    imgEl.title = imgEl.alt;
+  });
+}
+
+function addLazyloadToImg() {
+  const lazyImages = document.querySelectorAll(".gallery__image");
+  if ("loading" in HTMLImageElement.prototype) {
+    lazyImages.forEach((imgEl) => {
+      imgEl.loading = "lazy";
+    });
+  } else {
+    const lazyScript = document.createElement("script");
+
+    // lazyScript.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+    // lazyScript.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+    // lazyScript.crossorigin = "anonymous";
+    // lazyScript.referrerpolicy = "no-referrer";
+    document.body.appendChild(lazyScript);
+  }
+}

7f2ab1ee40d92a4f22c5764385adb65cdc617b04
Dranyk <dvdranyk@gmail.com>
2022-11-18 10:59:39 +0200

    3 - done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const checkLocalStorage = key => {
+    return !!localStorage.getItem(key);
+  };
+  
+  const fillTheFieldsFromLocalStorage = (el, localStorageKey) => {
+    if (!checkLocalStorage(localStorageKey)) return;
+  
+    const objFromStorage = JSON.parse(localStorage.getItem(localStorageKey));
+  
+    for (const key in objFromStorage) {
+      if (Object.hasOwnProperty.call(objFromStorage, key)) {
+        el.elements[key].value = objFromStorage[key];
+      }
+    }
+  };
+  
+  const onInput = (evt, objFromStorage = {}) => {
+    const key = 'feedback-form-state';
+  
+    if (checkLocalStorage(key))
+      objFromStorage = JSON.parse(localStorage.getItem(key));
+  
+    objFromStorage[evt.target.name] = evt.target.value;
+    localStorage.setItem(key, JSON.stringify(objFromStorage));
+  };
+
+const onSubmit = evt => {
+    const key = 'feedback-form-state';
+  
+    const resultToConsole = checkLocalStorage(key)
+      ? JSON.parse(localStorage.getItem(key))
+      : { email: '', message: '' }; 
+  
+    console.log(resultToConsole);
+  
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(key);
+  };
+  
+  fillTheFieldsFromLocalStorage(form, 'feedback-form-state');
+  form.addEventListener('input', throttle(onInput, 500));
+  form.addEventListener('submit', onSubmit);
